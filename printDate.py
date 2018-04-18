c4e91a846f4e5e89326900689c2d66da6b22211a
Scott Oonk <scott.oonk@gmail.com>
2018-04-17 20:04:32 -0400

    Take 1 image a second for 60 seconds

---
+++ printDate.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+d = datetime.now()
+print d.strftime("%Y%m%d_%H%M%S.%f")
---
+++ takeManyPictures.py
@@ -0,0 +1,15 @@
+from time import sleep
+from datetime import datetime
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+# warm-up
+sleep(2)
+
+for x in range(60):
+    sleep(1)
+    d = datetime.now()
+    filename = d.strftime("data/pic_%Y%m%d_%H%M%S.%f.jpg")
+    camera.capture(filename)
